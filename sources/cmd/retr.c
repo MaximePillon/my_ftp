7d8c64f68b09856539f4149fa609f777fb810761
Maxime Pillon <maxime.pillon@epitech.eu>
2017-05-19 15:39:59 +0200

    sources

--- sources/cmd/retr.c
+++ sources/cmd/retr.c
@@ -8,19 +8,51 @@
 ** Last update Wed May 10 14:33:12 2017 Maxime PILLON
 */
 
+#include		<string.h>
+#include		<stdlib.h>
+#include		<unistd.h>
+#include		<fcntl.h>
 #include		"server.h"
 
-int			push_data(int consocket, t_child *child, int serv_socket)
+int			push_data(int consocket, t_child *child, int serv_socket, int file)
 {
-  (void)consocket;
-  (void)child;
-  (void)serv_socket;
-  return (1);
+  char			buf[1024];
+  ssize_t		size;
+
+  size = read(file, buf, 1024);
+  while (size > 0)
+  {
+    write(consocket, buf, (size_t)size);
+    size = read(file, buf, 1024);
+  }
+  close(file);
+  close(consocket);
+  respond("226", "Closing data connection.", serv_socket);
+  free(child->data);
+  child->data = NULL;
+  return (0);
+}
+
+static int		communication(int consocket, t_child *child, int file)
+{
+  int			datasocket;
+
+  respond("150", "Data connection already open; transfer starting.", consocket);
+  if (child->mode == PASSIVE)
+  {
+    datasocket = accept(child->data->data_socket,
+			(struct sockaddr *) &(child->data->dest),
+			&(child->data->socksize));
+    return (push_data(datasocket, child, consocket, file));
+  }
+  connect(child->data->data_socket, (struct sockaddr*) &child->data->serv,
+	  child->data->socksize);
+  return (push_data(child->data->data_socket, child, consocket, file));
 }
 
 int			retr(int consocket, t_child *child)
 {
-  int			datasocket;
+  int			file;
 
   if (!is_authenticated(child))
   {
@@ -32,15 +64,12 @@ int			retr(int consocket, t_child *child)
     respond("425", "425 Use PORT or PASV first.", consocket);
     return (0);
   }
-  respond("150", "Data connection already open; transfer starting.", consocket);
-  if (child->mode == PASSIVE)
+  clear_arg(child->command);
+  file = open(child->command + 5, O_RDONLY);
+  if (file == -1)
   {
-    datasocket = accept(child->data->data_socket,
-			(struct sockaddr *) &(child->data->dest),
-			&(child->data->socksize));
-    return (push_data(datasocket, child, consocket));
+    respond("550", "Requested action not taken.", consocket);
+    return (-1);
   }
-  connect(child->data->data_socket, (struct sockaddr*) &child->data->serv,
-	  child->data->socksize);
-  return (push_data(child->data->data_socket, child, consocket));
+  return communication(consocket, child, file);
 }
--- sources/cmd/stor.c
+++ sources/cmd/stor.c
@@ -8,10 +8,59 @@
 ** Last update Wed May 10 14:33:48 2017 Maxime PILLON
 */
 
+#include		<string.h>
+#include		<stdlib.h>
+#include		<unistd.h>
+#include		<fcntl.h>
 #include		"server.h"
+
+int			read_data(int consocket, t_child *child, int serv_socket, int file)
+{
+  char			buf[1024];
+  char			buf1[1024];
+  ssize_t		size;
+  ssize_t		size1;
+
+  size = read(consocket, buf, 1024);
+  while (size > 0)
+  {
+    size1 = read(consocket, buf1, 1024);
+    if (size1 > 0)
+      write(file, buf, (size_t)size);
+    else
+      write(file, buf, ((size_t)size - 2));
+    strncpy(buf, buf1, (size_t)size1);
+    size = size1;
+  }
+  close(file);
+  close(consocket);
+  respond("226", "Closing data connection.", serv_socket);
+  free(child->data);
+  child->data = NULL;
+  return (0);
+}
+
+static int		communication(int consocket, t_child *child, int file)
+{
+  int			datasocket;
+
+  respond("150", "Data connection already open; transfer starting.", consocket);
+  if (child->mode == PASSIVE)
+  {
+    datasocket = accept(child->data->data_socket,
+			(struct sockaddr *) &(child->data->dest),
+			&(child->data->socksize));
+    return (read_data(datasocket, child, consocket, file));
+  }
+  connect(child->data->data_socket, (struct sockaddr*) &child->data->serv,
+	  child->data->socksize);
+  return (read_data(child->data->data_socket, child, consocket, file));
+}
 
 int			stor(int consocket, t_child *child)
 {
+  int			file;
+
   if (!is_authenticated(child))
   {
     respond("530", "Not logged in.", consocket);
@@ -22,5 +71,14 @@ int			stor(int consocket, t_child *child)
     respond("425", "425 Use PORT or PASV first.", consocket);
     return (0);
   }
-  return (0);
+  clear_arg(child->command);
+  file = open(child->command + 5,
+	      O_WRONLY | O_CREAT | O_TRUNC,
+	      S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  if (file == -1)
+  {
+    respond("550", "Requested action not taken.", consocket);
+    return (-1);
+  }
+  return communication(consocket, child, file);
 }
