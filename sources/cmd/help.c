6dec3d6a79d77c6c77558f29333a5c9335f89318
Maxime Pillon <maxime.pillon@epitech.eu>
2017-05-20 18:01:03 +0200

    help correct

--- sources/cmd/help.c
+++ sources/cmd/help.c
@@ -13,11 +13,11 @@
 
 int			help(int consocket, t_child *child)
 {
-  respond("21", "Help message.", consocket);
+  respond("214", "Help message.", consocket);
   dprintf(consocket, " USER PASS CWD CDUP QUIT DELE\n");
   dprintf(consocket, " PASV PORT HELP NOOP RETR STOR\n");
   dprintf(consocket, " SMNT STOU RMD MKD LIST PWD SYST\r\n");
-  respond("21", "Help message.", consocket);
+  respond("214", "Help message.", consocket);
   (void)child;
   return (0);
 }
